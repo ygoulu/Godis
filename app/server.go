2e195a2e9c3a86119923d4eb22a5adaaa849e15d
ygoulu <yago.goulu@gmail.com>
2024-03-11 20:50:16 -0300

    3rd stage

--- app/server.go
+++ app/server.go
@@ -18,12 +18,15 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	connection, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+
+	for {
+		connection, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+		go handleConn(connection)
 	}
-	handleConn(connection)
 }
 
 func handleConn(conn net.Conn) {
